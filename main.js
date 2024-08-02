9432646ba21d8f884566b6fcce6be8cf1347202f
SamPlayz6 <44670760+SamPlayz6@users.noreply.github.com>
2024-08-02 12:14:25 +0100

    First working example

--- main.js
+++ main.js
@@ -7,40 +7,21 @@ const fs = require('fs');
 let mainWindow;
 
 function createWindow() {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-
-  mainWindow = new BrowserWindow({
-    width: width,
-    height: height,
-    x: 0,
-    y: 0,
-    transparent: true,
-    frame: false,
-    alwaysOnTop: false,
-    skipTaskbar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
-
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    })
-  );
-
-  mainWindow.setIgnoreMouseEvents(true);
-
-  // Set the window as the desktop wallpaper
-  exec('powershell.exe -command "& {$w=(New-Object -ComObject Shell.Application).Windows() | Where-Object { $_.Type -eq 2 }; $w.Document.ActiveView.Focus(); [void][System.Reflection.Assembly]::LoadWithPartialName(\'System.Windows.Forms\'); [System.Windows.Forms.SendKeys]::SendWait(\'^{ESCAPE}\')}"');
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-}
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+      }
+    });
+  
+    mainWindow.loadFile('index.html');
+  
+    mainWindow.on('closed', function () {
+      mainWindow = null;
+    });
+  }
 
 app.on('ready', createWindow);
 
